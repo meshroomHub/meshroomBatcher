f1c054fbdf5fab77111a401862b7b9c318e4d0cc
Alice Sonolet <alice.sonolet@gmail.com>
2026-07-09 22:32:44 +0200

    Add fake data to test entity page

--- python/pipelineBatcher/ui/entities.py
+++ python/pipelineBatcher/ui/entities.py
@@ -3,6 +3,7 @@ Entities Helper for the Pipeline Batcher UI
 """
 
 # ========== Py standard lib imports ==========
+from random import randint
 import logging
 from typing import Any
 from pathlib import Path
@@ -32,8 +33,99 @@ def get_entity(entity_id: str) -> CachedEntity:
     return CachedEntityById.get(entity_id)
 
 
-class EntityProvider:
-    ...
+class MockEntityProvider:
+    @staticmethod
+    def get_entities_tree(entity_type: str) -> list[dict]:
+        entity_tree = []
+        entity_tree.append({
+            "id"      : "seq_001",
+            "label"   : "Sequence 001",
+            "icon"    : "\ue638",
+            "children": [
+                {
+                    "id"      : "seq_001_shot_001",
+                    "label"   : "001_001",
+                    "icon"    : "\ue02c",
+                    "children": [],
+                }
+            ]
+        })
+        entity_tree.append({
+            "id"      : "seq_002",
+            "label"   : "Sequence 002",
+            "icon"    : "\ue638",
+            "children": [
+                {
+                    "id"      : "seq_002_shot_001",
+                    "label"   : "002_001",
+                    "icon"    : "\ue02c",
+                    "children": [],
+                },
+                {
+                    "id"      : "seq_002_shot_002",
+                    "label"   : "002_002",
+                    "icon"    : "\ue02c",
+                    "children": [],
+                },
+                {
+                    "id"      : "seq_002_shot_003",
+                    "label"   : "002_003",
+                    "icon"    : "\ue02c",
+                    "children": [],
+                }
+            ]
+        })
+        entity_tree.append({
+            "id"      : "master_A",
+            "label"   : "Master A",
+            "icon"    : "\ue53b",
+            "children": [{
+                "id"      : "master_a_seq_001",
+                "label"   : "Sequence 001",
+                "icon"    : "\ue638",
+                "children": [
+                    {
+                        "id"      : "A_seq_001_shot_001",
+                        "label"   : "A_001_001",
+                        "icon"    : "\ue02c",
+                        "children": [],
+                    },
+                    {
+                        "id"      : "A_seq_001_shot_002",
+                        "label"   : "A_001_002",
+                        "icon"    : "\ue02c",
+                        "children": [],
+                    }
+                ]
+            }]
+        })
+        return entity_tree
+
+    @staticmethod
+    def fetch_entities_by_group(entity_type: str, group_id: str) -> list[dict]:
+        tree = MockEntityProvider.get_entities_tree(entity_type)
+        entities = []
+        if group_id == "seq_001":
+            entities = tree[0]["children"]
+        elif group_id == "seq_002":
+            entities = tree[1]["children"]
+        elif group_id == "master_A":
+            entities = [{
+                "id"      : "master_a_seq_001",
+                "label"   : "Sequence 001",
+            }]
+        elif group_id == "master_a_seq_001":
+            entities = tree[2]["children"][0]["children"]
+        group_entities = []
+        for e in entities:
+            status = ("wtg", "rdy", "ip", "rev", "apr")[randint(0, 4)]
+            group_entities.append({
+                "id"         : e["id"],
+                "name"       : e["label"],
+                "status"     : status,
+                "description": f"Entity {e['id']}"
+            })
+        return group_entities
 
 
 def get_entities_tree(entity_type: str) -> list[dict]:
@@ -46,7 +138,7 @@ def get_entities_tree(entity_type: str) -> list[dict]:
     - children: child items
     """
 
-    return EntityProvider.get_entities_tree(entity_type)
+    return MockEntityProvider.get_entities_tree(entity_type)
 
  
 def fetch_entities_by_group(entity_type: str, group_id: str) -> list[dict]:
@@ -59,4 +151,4 @@ def fetch_entities_by_group(entity_type: str, group_id: str) -> list[dict]:
     - description: displayed in the Description column (optional)
     """
 
-    return EntityProvider.fetch_entities_by_group(entity_type, group_id)
+    return MockEntityProvider.fetch_entities_by_group(entity_type, group_id)
